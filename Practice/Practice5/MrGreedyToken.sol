68149892631dda4547b9417180ceb0b59c015273
Kyrylo Riabov <kyryl.ryabov@gmail.com>
2022-09-23 15:11:51 +0300

    Fix bugs with tokens number

--- Practice/Practice5/MrGreedyToken.sol
+++ Practice/Practice5/MrGreedyToken.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.12;
+pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "./SimpleToken.sol";
@@ -11,7 +11,8 @@ interface IMrGreedyToken {
 }
 
 contract MrGreedyToken is IMrGreedyToken, SimpleToken("MrGreedyToken", "MRG") {
-    address constant treasury_ = 0xD000000000000000000000000000000000000000;
+    address private constant TREASURY = 0xD000000000000000000000000000000000000000;
+    uint256 private constant TEN_TOKENS = 10 * 10 ** 6;
 
     function decimals() public pure override returns (uint8) {
         return 6;
@@ -20,24 +21,25 @@ contract MrGreedyToken is IMrGreedyToken, SimpleToken("MrGreedyToken", "MRG") {
     function transfer(address to, uint256 amount) public virtual override returns (bool) {
         address owner = _msgSender();
 
-        uint256 tenTokens_ = 10 * 10 ** decimals();
-
-        if (amount <= tenTokens_) {
-            _transfer(owner, treasury_, amount);
+        if (amount <= TEN_TOKENS) {
+            _transfer(owner, TREASURY, amount);
         } else {
-            _transfer(owner, treasury_, tenTokens_);
-            _transfer(owner, to, amount - tenTokens_);
+            _transfer(owner, TREASURY, TEN_TOKENS);
+            _transfer(owner, to, amount - TEN_TOKENS);
         }
         return true;
     }
 
 
-    function treasury() external view returns (address) {
-        return treasury_;
+    function treasury() external pure returns (address) {
+        return TREASURY;
     }
 
     function getResultingTransferAmount(uint256 amount_) external pure returns (uint256) {
-        return amount_ - 10 * decimals();
+        if (amount_ > TEN_TOKENS) {
+            return amount_ - TEN_TOKENS;
+        }
+        return 0;
     }
 
     function getAddress() external view virtual override returns (address) {
